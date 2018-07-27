3c503ee1f5a5c5cc6531d537200c0fed9afe8892
Michael Dřizga <michael.drizga@gmail.com>
2018-07-27 12:10:14 +0200

    Transcriptor, makeList
    Completed transcriptor
    Added makeList and moved scanner and formater blosck there

--- src/Slovnik/SeznamSlov.java
+++ src/Slovnik/SeznamSlov.java
@@ -1,36 +1,47 @@
 package Slovnik;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.Normalizer;
 import java.util.Formatter;
 import java.util.Scanner;
 
 public class SeznamSlov {
     private static String transScriptor(String s) {
-
+        s.toLowerCase(); //changes string to lowercase
+        Normalizer.normalize(s, Normalizer.Form.NFD); //normalizes string
+        s.replaceAll("\\p{InCOMBINING_DIACRITICAL_MARKS}+", ""); //replaces all diacritics with ""
+//        char[] arr = s.toCharArray();
+//        for (char el: arr) {
+//
+//        }
         return s;
     }
-
-    public static void main(String[] args) {
+    public static void makeList() {
         File file = new File("C:\\Tutorial\\Obesenec\\src\\Slovnik\\syn2010_lemma_cba.txt");
         try {
-            Scanner sc = new Scanner(file);
+            Scanner sc = new Scanner(file); //creates new scanner object
             try {
-                Formatter f = new Formatter("C:\\Tutorial\\Obesenec\\src\\Slovnik\\seznam.txt");
+                Formatter f = new Formatter("C:\\Tutorial\\Obesenec\\src\\Slovnik\\seznam.txt"); //creates new file
 
-                while (sc.hasNextLine()) {
+                while (sc.hasNextLine()) { //loops if scanner finds text on line
 
-                    String s = transScriptor(sc.findInLine());
-                    f.format("%s", s + "\r\n");
+                    String s = transScriptor(sc.findInLine()); //saves transcription to string s
+                    f.format("%s", s + "\r\n"); //saves transcription to file
 
                 }
-                sc.close();
+                sc.close(); //scanner closes file
             } catch (FileNotFoundException sce) {
                 sce.printStackTrace();
             }
         } catch (Exception fe) {
             System.out.println("Error");
         }
+    }
+
+    public static void main(String[] args) {
+
 //        if(file.exists()) {
 //            System.out.println(file.getName() +  " exists!");
 //        }
