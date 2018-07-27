a392a6b917c46a5e8629b29fdf676981ee4976f0
Michael Dřizga <michael.drizga@gmail.com>
2018-07-27 16:39:32 +0200

    Wanna play ?
    Added question sequence.

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,22 @@ import static Slovnik.SeznamSlov.makeList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Wan to play a Hangman ? yes/no");
-        String a = sc.nextLine();
-
+        int i = 1;
+        while (i < 100) {
+            System.out.println("Wan to play a Hangman ? yes/no");
+            String answer = sc.nextLine();
+            if (answer.equals(new String("no"))) {
+                System.out.println("Too bad.");
+                break;
+            } else if (answer.equals(new String("yes"))) {
+                System.out.println("Let's begin.");
+                break;
+            } else {
+                System.out.println(answer + " is not a valid command.");
+                i++;
+            }
+            continue;
+        }
+        System.out.println("Ok that's enough.");
     }
 }
