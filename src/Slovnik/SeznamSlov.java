bfaaab9f0324f824d6dd61d920b1d05773ca6ce7
Michael Dřizga <michael.drizga@gmail.com>
2018-07-27 11:13:23 +0200

    Formatter
    Created new formatter

--- src/Slovnik/SeznamSlov.java
+++ src/Slovnik/SeznamSlov.java
@@ -2,21 +2,40 @@ package Slovnik;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class SeznamSlov {
     private static String transScriptor(String s) {
+
         return s;
     }
 
     public static void main(String[] args) {
-        File file = new File("Slovnik/syn2010_lemma_cba.txt");
-//        Scanner sc = new Scanner();
-        if(file.exists()) {
-            System.out.println(file.getName() +  "exists!");
+        File file = new File("C:\\Tutorial\\Obesenec\\src\\Slovnik\\syn2010_lemma_cba.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            try {
+                Formatter f = new Formatter("C:\\Tutorial\\Obesenec\\src\\Slovnik\\seznam.txt");
+
+                while (sc.hasNextLine()) {
+
+                    String s = transScriptor(sc.findInLine());
+                    f.format("%s", s + "\r\n");
+
+                }
+                sc.close();
+            } catch (FileNotFoundException sce) {
+                sce.printStackTrace();
+            }
+        } catch (Exception fe) {
+            System.out.println("Error");
         }
-        else {
-            System.out.println("The file does not exist");
-        }
+//        if(file.exists()) {
+//            System.out.println(file.getName() +  " exists!");
+//        }
+//        else {
+//            System.out.println("The file does not exist");
+//        }
     }
 }
