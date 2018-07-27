868aafd4dbfcb5bc7279de044bd9e6fd8f682ba7
Michael Dřizga <michael.drizga@gmail.com>
2018-07-27 15:38:23 +0200

    Repaired
    Everything works

--- src/Slovnik/SeznamSlov.java
+++ src/Slovnik/SeznamSlov.java
@@ -5,44 +5,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.Normalizer;
 import java.util.Formatter;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SeznamSlov {
     private static boolean filter(String s) { //if string is only 3 characters long returns true
-        if (s.length() <= 3 ) {
+        if (s.length() <= 3) {
             return true;
         }
         return false;
     }
 
     private static String transScriptor(String s) {
-        s.toLowerCase(); //changes string to lowercase !!!NEFUNGUJE!!!
-        Normalizer.normalize(s, Normalizer.Form.NFD); //normalizes string !!!NEFUNGUJE!!!
-        s.replaceAll("\\p{InCOMBINING_DIACRITICAL_MARKS}+", ""); //replaces all diacritics with "" !!!NEFUNGUJE!!!
-//        char[] arr = s.toCharArray();
-//        for (char el: arr) {
-//
-//        }
-        return s;
+        s = s.toLowerCase(); //changes string to lowercase !!!NEFUNGUJE!!!
+        return Normalizer.normalize(s, Normalizer.Form.NFD) //normalizes string !!!NEFUNGUJE!!!
+                .replaceAll("\\p{InCOMBINING_DIACRITICAL_MARKS}+", ""); //replaces all diacritics with "" !!!NEFUNGUJE!!!
     }
 
-    public static void makeList() {
+
+    public static List<String> makeList() {
         File file = new File("syn2010_lemma_cba.txt");
-        String string;
+        List<String> l = new LinkedList();
         try {
-            Scanner sc = new Scanner(file,  "windows-1250"); //creates new scanner object
+            Scanner sc = new Scanner(file, "windows-1250"); //creates new scanner object
             while (sc.hasNextLine()) { //loops if scanner finds text on line
-                string = sc.findInLine("[\\p{IsLatin}]+"); //saves next string
+                String string = sc.findInLine("[\\p{IsLatin}]+"); //saves next string
                 if (filter(string)) {
                     string = sc.findInLine("[\\d]+"); //finds numbers in line
                     sc.nextLine();
                     continue;
                 }
-
-
                 String out = transScriptor(string); //saves transcription to string s
+                l.add(out);
 
-                System.out.println(out);
+
                 string = sc.findInLine("[\\d]+"); //finds numbers in line
                 sc.nextLine();
             }
@@ -51,15 +48,17 @@ public class SeznamSlov {
             System.out.println("File not found");
             sce.printStackTrace();
         }
+        return l;
     }
 
     public static void main(String[] args) {
-        makeList();
-//        if(file.exists()) {
-//            System.out.println(file.getName() +  " exists!");
-//        }
-//        else {
-//            System.out.println("The file does not exist");
-//        }
+        int i = 0;
+        for (String el : makeList()) {
+            i++;
+            System.out.println(el);
+            if (i > 10) {
+                break;
+            }
+        }
     }
 }
